21afd830db6b5c5efd8354e795673caa25a948c0
K Mak <karen.mak@stx.oxon.org>
2014-01-10 15:39:13 -0500

    circle circum. tests x2 passed

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,5 +1,17 @@
 module Geometry
   class Circle
+    attr_accessor :radius
+    def initialize
+      @radius = radius
+    end
+
+    def circumference
+      Math::PI * 2 * @radius
+    end
+
+    def area
+    end
+
   end
 end
 
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,1 +1,23 @@
 require 'spec_helper'
+require_relative '../lib/circle'
+
+
+describe Geometry::Circle do
+  describe '#circumference' do
+    it 'calculate the circumfrence by multiplying the diameter with PI' do
+      circle = Geometry::Circle.new
+      circle.radius = 4
+      expect(circle.circumference).to eq 25.132741228718345 # for circle with radius 4
+    end
+  end
+
+  describe '#circumference' do
+    it 'calculate the circumfrence by multiplying the diameter with PI' do
+      circle = Geometry::Circle.new
+      circle.radius = 12
+      expect(circle.circumference).to eq 75.39822368615503
+    end
+  end
+
+
+end
